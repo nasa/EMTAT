9fe6fd30197eff37533d51b703620d8dd4894d04
MarkBell1701C <62667199+MarkBell1701C@users.noreply.github.com>
2021-09-13 12:54:52 -0400

    Installation file

---
+++ Install_EMTAT.m
@@ -0,0 +1,39 @@
+
+%% Choose Instalation Type.
+IT = input('Choose EMTAT installation type:\n (1) Temporary Installation.\n (2) Permanent Installation.\n (3) Cancel Installation.\n');
+
+%% Install Logic.
+if IT == 1 %Temporary Install
+    
+    addpath(genpath(pwd)) %temporarily add current and sub-directories to path.
+    fprintf('Paths added.\n')
+    
+    lb = LibraryBrowser.LibraryBrowser2; %Simulink Library Browser
+    refresh(lb) %refresh Simulink Library Browser
+    fprintf('Simulink Library Refreshed.\n')
+    
+    show(lb) %Show Simulink Library Browser
+    
+    fprintf('EMTAT Temporarily Installed.\n')
+elseif IT == 2
+    path(pathdef) %restore paths to paths set at startup
+    addpath(genpath(pwd)) %temporarily add current and sub-directories to path.
+    SP = savepath; %save current matlab path to pathdef.m, which is read on startup.
+    
+    if SP == 0 %Ensure paths are permanently saved.
+        fprintf('Paths added and saved.\n')
+    else
+        error('Installation Error: Paths not saved.\n')
+    end
+    
+    lb = LibraryBrowser.LibraryBrowser2; %Simulink Library Browser
+    refresh(lb) %refresh Simulink Library Browser
+    
+    show(lb) %Show Simulink Library Browser
+    
+    fprintf('EMTAT Permanently Installed.\n')
+elseif IT == 3
+    fprintf('EMTAT Installation Canceled.\n')
+end
+
+clear IT lb SP
---
+++ Uninstall_EMTAT.m
@@ -0,0 +1,20 @@
+
+%% Remove directories and sub-directories from path.
+path(pathdef) %restore paths to paths set at startup
+rmpath(genpath(pwd)) %add current and sub-directories to path.
+SP = savepath;
+
+if SP == 0 %Ensure paths are permanently saved.
+    fprintf('Paths removed and saved.\n')
+else
+    error('Installation Error: Paths not saved.\n')
+end
+
+lb = LibraryBrowser.LibraryBrowser2; %Simulink Library Browser
+refresh(lb) %refresh Simulink Library Browser
+
+show(lb)
+
+clear lb SP
+
+fprintf('EMTAT Uninstalled. Please remove EMTAT directory from computer.\n')
